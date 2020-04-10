3e10c396461c484eb40f4375e37fbe5fed0c5b97
Martin Spusta <lucas.spusta@gmail.com>
2020-04-10 10:28:27 -0400

    Initial commit

---
+++ app/src/main/java/itkido/me/videoplayerkotlin/MainActivity.kt
@@ -0,0 +1,101 @@
+package itkido.me.videoplayerkotlin
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+    var playerView: PlayerView? = null
+    private var player: SimpleExoPlayer? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
+        setContentView(R.layout.activity_main)
+        playerView = findViewById(R.id.video_view)
+
+        initializePlayer()
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(this)
+        playerView!!.player = player
+        val uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/mentorpodium.appspot.com/o/posts%2FKKHWfCAX1hVoAYKF8RlPaCWCCsD2%2Fvideos%2Fsave_1585499153575.mp4?alt=media&token=79080691-3667-4ea6-b824-d13b3806d32b")
+        val mediaSource = buildMediaSource(uri)
+        player!!.playWhenReady = playWhenReady
+        player!!.seekTo(currentWindow, playbackPosition)
+        player!!.prepare(mediaSource, false, false)
+    }
+
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(this, "videoplayer")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(uri)
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playWhenReady = player!!.playWhenReady
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            player!!.release()
+            player = null
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView!!.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+
+}
